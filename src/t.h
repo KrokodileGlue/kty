eac475b3284dbf2d28089d97f8539b39ed497a23
KrokodileGlue <krokodileglue@outlook.com>
2022-12-07 04:56:34 -0500

    Clean up `t.h`

--- src/t.h
+++ src/t.h
@@ -1,18 +1,7 @@
 #ifndef T_H
 #define T_H
 
-#include <stdint.h>
-#include <stdbool.h>
-
-#include "esc.h"
-
 struct term;
-struct grid;
-
-/*
- * There should be only one of these functions available: twrite. The
- * rest should be internal.
- */
 
 int twrite(struct term *t, const char *buf, int buflen);
 void tresize(struct term *t, int col, int row);
